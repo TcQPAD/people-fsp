008dca73df7104a38fbf29d7ab4a273c790d4959
maxim <maxime.flament@etu.unice.fr>
2017-12-01 20:27:16 +0100

    Changed synchronization to lock in a fine-grained way Extracted Tile class to dedicated file Added responsibility to Tile class such as reading/writing content Extracted synchronization to Tile class Closes #10

---
+++ core/tile.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Defines a new class Tile used by the map
+to represent its cells.
+
+A Tile has (x, y) coordinates, a value in
+[|1, 3|] that represents the object present at the
+Tile coordinates, and several methods that
+are Thread-safe, in order to synchronize
+all the people on the map (where each person
+is actually represented by a Thread).
+"""
+
+from tile_value_enum import TileValueEnum
+from threading import Lock  # to synchronize threads
+
+
+class Tile:
+    """
+    contentEnum can be one of the following :
+    empty = 0
+    obstacle = 1
+    exit = 2
+    it can also be a Person object, but at initialization time,
+    contentEnum can only be one of the previous value.
+    """
+
+    def __init__(self, contentEnum):
+        """
+        The object that will do the magic to synchronize the calling threads,
+        i.e., the Person objects.
+        """
+        self.content = contentEnum
+        self.tileLock = Lock()
+
+    """
+    Returns the content of this Tile
+    """
+    def getContent(self):
+        self.tileLock.acquire()
+        tmp = self.content
+        self.tileLock.release()
+        return tmp
+
+    """
+    Sets the content of this Tile to the given value
+    """
+
+    def setContent(self, contentEnum):
+        self.tileLock.acquire()
+        self.content = contentEnum
+        self.tileLock.release()
+
+    '''
+        Returns true if the given cell has the given person in it
+        '''
+
+    def hasPerson(self, person):
+        self.tileLock.acquire()
+        hP = person == self.content
+        self.tileLock.release()
+        return hP
+
+    '''
+        Returns true if the given person has reached the exit cell
+        '''
+
+    def isAtExit(self):
+        self.tileLock.acquire()
+        tmp = self.content == TileValueEnum.exit
+        self.tileLock.release()
+        return tmp
+
+    '''
+    Returns true if the given cell contains a person
+    '''
+
+    def isCellTaken(self):
+        self.tileLock.acquire()
+        tmp = self.content != TileValueEnum.empty and self.content != TileValueEnum.obstacle
+        self.tileLock.release()
+        return tmp
+
+    '''
+    Returns true if the given Tile has an obstacle
+    '''
+
+    def isObstacle(self):
+        self.tileLock.acquire()
+        tmp = self.content == TileValueEnum.obstacle
+        self.tileLock.release()
+        return tmp
+
+    '''
+    Returns true if the given Tile is an exit
+    '''
+
+    def isExit(self):
+        self.tileLock.acquire()
+        tmp = self.content == TileValueEnum.exit
+        self.tileLock.release()
+        return tmp
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from core.display import Display
     Starts the project by outputting information about the processes only,
     without UI. 
 '''
+
+
 def noUI():
     map = Map()
     algorithm = Algorithm(map, nbP)
@@ -16,10 +18,13 @@ def noUI():
     print("Starting project with no UI!")
     return
 
+
 '''
     Starts the project by outputting information about the processes and by
     outputting through a UI. 
 '''
+
+
 def yesUI():
     display = Display(550, 350)
     map = Map(display)
@@ -29,21 +34,25 @@ def yesUI():
     print("Starting project with UI!")
     return
 
+
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--showUi", 
-        type=str, 
+        "--showUi",
+        type=str,
         default="true",
-        help="Valeur booléenne spécifiant si la partie graphique devrait être affichée ou non. \nValeur par défaut : faux." 
-        )
+        help="Valeur booléenne spécifiant si la partie graphique devrait être affichée ou non. \nValeur par défaut : "
+             "faux. "
+    )
 
     parser.add_argument(
         "-p",
         type=int,
         default=4,
-        help="Puissance de 2 pour le nombre de personnes à insérer dans la simulation. Valeur par défaut: 4 (donc 2^4 = 16 personnes)."
+        help="Puissance de 2 pour le nombre de personnes à insérer dans la simulation. Valeur par défaut: 4 (donc 2^4 "
+             "= 16 personnes). "
     )
 
     # récupère les arguments dans un objet (appelable comme un struct en C)
@@ -52,7 +61,7 @@ if __name__ == '__main__':
     nbP = 4
 
     if args.p:
-        if args.p <= 512*128:
+        if args.p <= 512 * 128:
             nbP = args.p
         else:
             raise Exception("Too many people in args, using 4 persons instead")
@@ -60,14 +69,14 @@ if __name__ == '__main__':
     if args.showUi:
 
         # create an object that inputs data randomly 
-        if args.showUi == "true" :
+        if args.showUi == "true":
             yesUI()
             exit(0)
 
-        else :
+        else:
             noUI()
             exit(0)
 
-    else :
+    else:
         noUI()
         exit(0)
