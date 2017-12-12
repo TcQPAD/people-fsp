c7cdad6f959e7f09c232850a0c2318785564ac8f
maxim <maxime.flament@etu.unice.fr>
2017-12-12 18:03:19 +0100

    Revert "Séparation de la partie save/load d'une map avec la classe Map + on peut charger une map pour le scénario 2"
    This reverts commit a7953e6 because it is bugged as fuck

--- core/map/map_first_scenario.py
+++ core/map/map_first_scenario.py
@@ -9,6 +9,12 @@ from core.constants.tile_value_enum import TileValueEnum
 from core.map.obstacle import Obstacle
 from core.threads.person_first_scenario import PersonFirstScenario
 from tile import Tile
+
+"""
+Class to describe the map
+"""
+NAME_MAP = "map.txt"
+
 '''
     Class to describe the map
 '''
@@ -26,6 +32,46 @@ class MapFirstScenario(AbstractMap):
         self.loadedMap = loadedMap
         self.fillMap()
         self.draw()
+
+    def saveMap(self, persons):
+        fileMap = open(NAME_MAP, "w")
+
+        for obstacle in self.obstacleList:
+            fileMap.write(
+                str(obstacle.x1) + " " + str(obstacle.x2) + " " + str(obstacle.y1) + " " + str(obstacle.y2) + "\n")
+
+        fileMap.write("#\n")
+
+        for person in persons:
+            fileMap.write(str(person.x) + " " + str(person.y) + " " + str(person.threadId) + "\n")
+
+        fileMap.close()
+
+    '''
+    Load a .txt file containing a map. This file gets the informations about the position of the obstacles and the persons
+    '''
+
+    def loadMap(self):
+        fileMap = open(NAME_MAP, "r")
+        line = fileMap.readline()
+
+        parseObstacle = True
+
+        while line:
+            if line == "#\n":
+                parseObstacle = False
+                line = fileMap.readline()
+                continue
+
+            coordonnee = line.split(' ')
+            if parseObstacle:
+                self.obstacleList.append(
+                    Obstacle(int(coordonnee[0]), int(coordonnee[1]), int(coordonnee[2]), int(coordonnee[3])))
+            else:
+                self.personList.append(PersonFirstScenario(None, int(coordonnee[0]), int(coordonnee[1]), int(coordonnee[2]), self.barrier))
+
+            line = fileMap.readline()
+        fileMap.close()
 
     @property
     def getPersons(self):
